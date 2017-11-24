e20cd45ab82bfc71dc3651d3c56bb2e9f3b2ed1a
焦雁飞 <911306137@qq.com>
2017-11-24 15:54:38 +0800

    添加任务广场

---
+++ src/routes/TaskSquare/TaskCard.js
@@ -0,0 +1,27 @@
+import React, { PureComponent } from 'react';
+import { connect } from 'dva';
+
+@connect(state => ({
+
+}))
+
+export default class FlowList extends PureComponent {
+  state = {
+   
+  }
+
+  componentDidMount() {
+    
+  }
+  componentWillReceiveProps(nextProps) {
+    
+  }
+
+  
+  render() {
+    
+    return (
+      <div />
+    );
+  }
+}
