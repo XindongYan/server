cc4d22589d7c23e33c47face0a88a3f5b3fd8416
apple <apple@appledeMBP.lan>
2017-11-27 18:00:55 +0800

    html

--- src/routes/TaskSquare/ProjectCard.js
+++ src/routes/TaskSquare/ProjectCard.js
@@ -29,11 +29,18 @@ export default class ProjectCard extends PureComponent {
     const { project } = this.props;
     return (
       <Col span={8} onClick={() => this.projectInto(project)}>
-        <Card title={<TaskTitleColumn text={project.title} length={14}/>} className={styles.cardCol}>
-          <p title={project.merchant_tag}>商家标签：{ project.merchant_tag ? project.merchant_tag : '无' }</p>
-          <p title={project.desc}>项目描述：{ project.desc ? project.desc : '无' }</p>
-          <p>截止日期：{ project.deadline ? moment(project.deadline).format('YYYY-MM-DD HH:mm:ss') : '无' }</p>
-          <p>项目奖励：{ project.price ? project.price : '无' }</p>
+        <Card className={styles.cardCol} bodyStyle={{ padding: 0 }}>
+          <div className={styles.cardColTop}>
+            <h3 title={project.title}>{project.title}</h3>
+            <p title={project.merchant_tag}>{ project.merchant_tag ? project.merchant_tag : '无' }</p>
+          </div>
+          <div className={styles.cardColBottom}>
+            <p className={styles.cardMsgDesc}>项目描述：{ project.desc ? project.desc : '无' }</p>
+            <div className={styles.cardMsgB}>
+              <span className={styles.cardMsgPrice}>报酬：{ project.price ? project.price : '无' }</span>
+              <span className={styles.cardMsgDeadline}>{ project.deadline ? moment(project.deadline).format('YYYY-MM-DD HH:mm:ss') : '无' }</span>
+            </div>
+          </div>
         </Card>
       </Col>
     );
