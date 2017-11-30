b7c9b8573e8d91e0bcb21b6d025ef9a6afdf3a4d
apple <apple@appledeMBP.lan>
2017-11-30 13:52:41 +0800

    html

--- src/routes/Approver/TaskEdit.js
+++ src/routes/Approver/TaskEdit.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'dva';
 import querystring from 'querystring';
-import { Card, Button, message } from 'antd';
+import { Card, Button, message, Popover, Slider } from 'antd';
 import Editor from '../../components/Editor';
 import TaskChat from '../../components/TaskChat';
 import styles from './TableList.less';
@@ -21,6 +21,11 @@ export default class TaskEdit extends PureComponent {
       task_desc: '',
       cover_img: '',
     },
+    grades: [
+      {name: '标题', value: 0},
+      {name: '正文', value: 0},
+      {name: '图片', value: 0},
+    ]
   }
   componentDidMount() {
     const query = querystring.parse(this.props.location.search.substr(1));
@@ -38,16 +43,77 @@ export default class TaskEdit extends PureComponent {
       }
     });
   }
+  changeGrades = (index, value) => {
+    const newGrades = [...this.state.grades];
+    newGrades.splice(index, 1, {...this.state.grades[index], value: value});
+    this.setState({
+      grades: newGrades
+    })
+  }
   handleChange = (task) => {
     this.setState({ task: { ...this.state.task, ...task } });
   }
-  handleSubmit = () => {
-    
+  handleSave = () => {
+    console.log(this.state.task);
+    const query = querystring.parse(this.props.location.search.substr(1));
+    this.props.dispatch({
+      type: 'task/update',
+      payload: { ...this.state.task, _id: query._id },
+      callback: (result) => {
+        if (result.error) {
+          message.error(result.msg);
+        } else {
+          message.success(result.msg);
+        }
+      }
+    });
   }
+  handleSubmit = () => {
+    console.log(this.state.task);
+    const query = querystring.parse(this.props.location.search.substr(1));
+    this.props.dispatch({
+      type: 'task/update',
+      payload: { ...this.state.task, _id: query._id },
+      callback: (result) => {
+        if (result.error) {
+          message.error(result.msg);
+        } else {
+          this.props.dispatch({
+            type: 'task/approve',
+            payload: { _id: query._id },
+            callback: (result1) => {
+              if (result1.error) {
+                message.error(result1.msg);
+              } else {
+                message.success(result1.msg);
+              }
+            }
+          });
+        }
+      }
+    });
+  }
+
   render() {
     const { formData } = this.props;
-    console.log(formData.approve_step)
+    const { grades } = this.state;
     const operation = !formData.approve_step ? 'edit' : 'view';
+    const content = (
+      <div style={{width: 360}}>
+        {grades.map((item, index) => 
+          <div style={{padding: '10px 0'}} key={index}>
+            <span style={{margin: '0 15px'}}>{item.name}</span>
+            <Slider
+              style={{width: '80%', display: 'inline-block', margin: 0}}
+              value={item.value}
+              max={10}
+              step={0.1}
+              onChange={(value) => this.changeGrades(index, value)}
+            />
+          </div>)
+        }
+      </div>
+    );
     // const taskOuterBoxHeight = $(this.refs.taskOuterBox).outerHeight() || 0;
     return (
       <Card bordered={false} title="" style={{ background: 'none' }} bodyStyle={{ padding: 0 }}>
@@ -57,7 +123,12 @@ export default class TaskEdit extends PureComponent {
             <Annotation viewStatus="edit" value={[]} />
           </div>
           <div className={styles.submitBox}>
-            <Button onClick={this.handleSubmit}>提交</Button>
+            <Popover content={content} title="评分" trigger="hover">
+              <Button onClick={this.handleSubmit}>不通过</Button>
+            </Popover>
+            <Popover content={content} title="评分" trigger="hover">
+              <Button onClick={this.handleSubmit}>通过</Button>
+            </Popover>
             <Button onClick={this.handleSave}>保存</Button>
           </div>
         </div>
