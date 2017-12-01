6d67e36f47f2052fc3fd0f104c07782371aeeea5
apple <apple@appledeMBP.lan>
2017-12-01 11:01:56 +0800

    Annotation

--- src/routes/Approver/TaskView.js
+++ src/routes/Approver/TaskView.js
@@ -41,7 +41,7 @@ export default class TaskView extends PureComponent {
         title: nextProps.formData.title,
         task_desc: nextProps.formData.task_desc,
         cover_img: nextProps.formData.cover_img,
-        approve_notes: nextProps.formData.approve_notes,
+        approve_notes: nextProps.formData.approve_notes || [],
       }
     });
   }
