becf3541d0228721a4579e2dfbfcad670daa150b
焦雁飞 <911306137@qq.com>
2017-12-05 19:20:56 +0800

    修改用户信息密码

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,4 @@
-import { query as queryUsers, queryCurrent } from '../services/user';
+import { query as queryUsers, queryCurrent, updateUser, changePassword } from '../services/user';
 
 export default {
   namespace: 'user',
@@ -34,6 +34,14 @@ export default {
         type: 'saveCurrentUser',
         payload: response,
       });
+      if (callback) callback(response);
+    },
+    *update({ payload, callback }, { call, put }) {
+      const response = yield call(updateUser, payload);
+      if (callback) callback(response);
+    },
+    *changePassword({ payload, callback }, { call, put }) {
+      const response = yield call(changePassword, payload);
       if (callback) callback(response);
     },
   },
--- src/services/user.js
+++ src/services/user.js
@@ -3,3 +3,21 @@ import request from '../utils/request';
 export async function queryCurrent() {
   return request('/api/user/currentUser');
 }
+
+export async function updateUser(params) {
+  return request('/api/user', {
+    method: 'PUT',
+    body: {
+      ...params,
+    },
+  });
+}
+
+export async function changePassword(params) {
+  return request('/api/user/change/pwd', {
+    method: 'PUT',
+    body: {
+      ...params,
+    },
+  });
+}
