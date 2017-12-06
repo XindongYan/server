709fd4d948a5b27a9aacbb013291c78ae2657a53
apple <apple@appledeMBP.lan>
2017-12-06 16:11:34 +0800

    修改密码

--- src/routes/User/UserPassWord.js
+++ src/routes/User/UserPassWord.js
@@ -64,7 +64,7 @@ export default class PassWord extends Component {
                 message.error(result.msg);
               } else {
                 message.success(result.msg);
-                this.props.dispatch(routerRedux.push('/setting/userInfo'));
+                // this.props.dispatch(routerRedux.push('/setting/userInfo'));
               }
             },
           });
@@ -89,7 +89,6 @@ export default class PassWord extends Component {
                 message.error(result.msg);
               } else {
                 message.success(result.msg);
-                // this.props.dispatch(routerRedux.push('/setting/userInfo'));
               }
             },
           });
@@ -130,110 +129,122 @@ export default class PassWord extends Component {
         <div className={styles.top}>
           修改密码
         </div>
-        <Form style={{ display: type === 'pass' ? 'block' : 'none' }}>
-          <FormItem
-            {...formItemLayout}
-            label="旧密码"
-            style={{ padding: '0 20px' }}
-          >
-            {getFieldDecorator('oldPsd', {
-              rules: [{
-                required: true, message: '请输入旧密码!',
-              }],
-            })(
-              <Input type="password" />
-            )}
-          </FormItem>
-          <FormItem
-            {...formItemLayout}
-            label="新密码"
-            style={{ padding: '0 20px' }}
-          >
-            {getFieldDecorator('newPsd', {
-              rules: [{
-                required: true, message: '请输入新密码!',
-              }],
-            })(
-              <Input type="password" />
-            )}
-          </FormItem>
-          <FormItem
-            {...formItemLayout}
-            label="重复密码"
-            style={{ padding: '0 20px' }}
-          >
-            {getFieldDecorator('againPsd', {
-              rules: [{
-                required: true, message: '请确认新密码!',
-              }, {
-                validator: this.checkConfirm,
-              }],
-            })(
-              <Input type="password" />
-            )}
-          </FormItem>
-          <FormItem {...tailFormItemLayout}>
-            <Button type="primary" onClick={this.handleSubmit}>保存</Button>
-          </FormItem>
-        </Form>
-
-        <Form style={{ display: type === 'phone' ? 'block' : 'none', padding: '0 40px' }}>
-          <FormItem>
-            {getFieldDecorator('phone', {
-              rules: [{
-                required: type === 'mobile', message: '请输入手机号！',
-              }, {
-                pattern: /^1\d{10}$/, message: '手机号格式错误！',
-              }],
-            })(
-              <Input
-                size="large"
-                prefix={<Icon type="mobile" className={styles.prefixIcon} />}
-                placeholder="手机号"
-              />
-            )}
-          </FormItem>
-          <FormItem>
-            <Row gutter={8}>
-              <Col span={16}>
-                {getFieldDecorator('sms_code', {
-                  rules: [{
-                    required: type === 'mobile', message: '请输入验证码！',
-                  }],
-                })(
-                  <Input
-                    size="large"
-                    prefix={<Icon type="mail" className={styles.prefixIcon} />}
-                    placeholder="验证码"
-                  />
-                )}
-              </Col>
-              <Col span={8}>
-                <Button
-                  disabled={count}
-                  className={styles.getCaptcha}
+        { type === 'pass' &&
+          <Form>
+            <FormItem
+              {...formItemLayout}
+              label="旧密码"
+              style={{ padding: '0 20px' }}
+            >
+              {getFieldDecorator('oldPsd', {
+                rules: [{
+                  required: true, message: '请输入旧密码!',
+                }],
+              })(
+                <Input type="password" />
+              )}
+            </FormItem>
+            <FormItem
+              {...formItemLayout}
+              label="新密码"
+              style={{ padding: '0 20px' }}
+            >
+              {getFieldDecorator('newPsd', {
+                rules: [{
+                  required: true, message: '请输入新密码!',
+                }],
+              })(
+                <Input type="password" />
+              )}
+            </FormItem>
+            <FormItem
+              {...formItemLayout}
+              label="重复密码"
+              style={{ padding: '0 20px' }}
+            >
+              {getFieldDecorator('againPsd', {
+                rules: [{
+                  required: true, message: '请确认新密码!',
+                }, {
+                  validator: this.checkConfirm,
+                }],
+              })(
+                <Input type="password" />
+              )}
+            </FormItem>
+            <FormItem {...tailFormItemLayout}>
+              <Button type="primary" onClick={this.handleSubmit}>保存</Button>
+            </FormItem>
+          </Form>
+        }
+        { type === 'phone' &&
+          <Form>
+            <FormItem>
+              {getFieldDecorator('phone', {
+                rules: [{
+                  required: type === 'mobile', message: '请输入手机号！',
+                }, {
+                  pattern: /^1\d{10}$/, message: '手机号格式错误！',
+                }],
+              })(
+                <Input
                   size="large"
-                  onClick={this.onGetCaptcha}
-                >
-                  {count ? `${count} s` : '获取验证码'}
-                </Button>
-              </Col>
-            </Row>
-          </FormItem>
-          <FormItem>
-            {getFieldDecorator('password', {
-              rules: [{ required: true, message: 'Please input your Password!' }],
-            })(
-              <Input
-                size="large"
-                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password"
-              />
-            )}
-          </FormItem>
-          <FormItem {...tailFormItemLayout}>
-            <Button type="primary" onClick={this.handleSubmitPhone}>保存</Button>
-          </FormItem>
-        </Form>
+                  prefix={<Icon type="mobile" className={styles.prefixIcon} />}
+                  placeholder="手机号"
+                />
+              )}
+            </FormItem>
+            <FormItem>
+              <Row gutter={8}>
+                <Col span={16}>
+                  {getFieldDecorator('sms_code', {
+                    rules: [{
+                      required: type === 'mobile', message: '请输入验证码！',
+                    }],
+                  })(
+                    <Input
+                      size="large"
+                      prefix={<Icon type="mail" className={styles.prefixIcon} />}
+                      placeholder="验证码"
+                    />
+                  )}
+                </Col>
+                <Col span={8}>
+                  <Button
+                    disabled={count}
+                    className={styles.getCaptcha}
+                    size="large"
+                    onClick={this.onGetCaptcha}
+                  >
+                    {count ? `${count} s` : '获取验证码'}
+                  </Button>
+                </Col>
+              </Row>
+            </FormItem>
+            <FormItem>
+              {getFieldDecorator('password', {
+                rules: [{ required: true, message: 'Please input your Password!' }],
+              })(
+                <Input
+                  size="large"
+                  prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password"
+                />
+              )}
+            </FormItem>
+            <FormItem>
+              <Row gutter={8}>
+                <Col span={16} style={{ textAlign: 'center' }}>
+                  <Button type="primary" onClick={this.handleSubmitPhone}>保存</Button>
+                </Col>
+                <Col span={8} style={{ textAlign: 'center' }}>
+                  <Link to="/user/login">
+                    <span>去登陆</span>
+                  </Link>
+                </Col>
+              </Row>
+            </FormItem>
+          </Form>
+        }
       </div>
     );
   }
