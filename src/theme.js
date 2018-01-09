66aa6ff8900c5f302bb0859b87bc40419fa6c565
焦雁飞 <911306137@qq.com>
2018-01-09 15:00:49 +0800

    主题

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,5 @@
 // https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
 module.exports = {
-  // 'primary-color': '#10e99b',
+  'primary-color': '#00b395',
   'card-actions-background': '#f5f8fa',
 };
