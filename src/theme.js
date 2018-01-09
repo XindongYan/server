71eb45222c76d7061c766ab183979b461b107738
焦雁飞 <911306137@qq.com>
2018-01-09 15:24:52 +0800

    主题

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,10 @@
 // https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
 module.exports = {
   'primary-color': '#00b395',
+  'link-color': '#1890ff',
+  'link-hover-color': 'rgba(24,144,255, 0.8)',
+  'link-active-color': 'rgba(24,144,255, 0.8)',
+  'link-decoration': 'none',
+  'link-hover-decoration': 'none',
   'card-actions-background': '#f5f8fa',
 };
