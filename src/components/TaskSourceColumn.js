295e2f74d3eb6b165517e0a7b45060288da78dd6
焦雁飞 <911306137@qq.com>
2018-01-26 15:49:36 +0800

    修复bug

--- src/components/TaskSourceColumn.js
+++ src/components/TaskSourceColumn.js
@@ -4,15 +4,15 @@ import { SOURCE } from '../constants';
 
 export default class TaskSourceColumn extends PureComponent {
   render() {
-    const { source } = this.props;
+    const { source, text } = this.props;
     if (source === SOURCE.task) {
-      return (<Badge status="processing" text="赏金任务" />);
+      return (<Badge status="processing" text={text || "赏金任务"} />);
     } else if (source === SOURCE.deliver) {
-      return (<Badge status="warning" text="投稿" />);
+      return (<Badge status="warning" text={text || "投稿"} />);
     } else if (source === SOURCE.create) {
-      return (<Badge status="success" text="我的" />);
+      return (<Badge status="success" text={text || "我的"} />);
     } else if (source === SOURCE.pass) {
-      return (<Badge status="error" text="转交" />);
+      return (<Badge status="error" text={text || "转交"} />);
     } else {
       return <span>{source}</span> ;
     }
