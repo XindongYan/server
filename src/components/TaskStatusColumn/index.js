a23fd2f2a0c61b0dd16d8c566cb066ef44f3c3e5
焦雁飞 <911306137@qq.com>
2018-02-03 17:56:10 +0800

    修改状态颜色

--- src/components/TaskStatusColumn/index.js
+++ src/components/TaskStatusColumn/index.js
@@ -10,7 +10,7 @@ export default class TaskStatusColumn extends PureComponent {
     } else if (status === -2) {
       return (<Tag className={styles['ant-tag']} color="#C1C1C1">已上架</Tag>);
     } else if (status === -1) {
-      return (<Tag className={styles['ant-tag']} color="#FFC125">已接单</Tag>);
+      return (<Tag className={styles['ant-tag']} color="#00b395">已接单</Tag>);
     } else if (status === 0) {
       return (<Tag className={styles['ant-tag']} color="#108ee9">待审核</Tag>);
     } else if (status === 1) {
@@ -18,9 +18,9 @@ export default class TaskStatusColumn extends PureComponent {
     } else if (status === 2) {
       return (<Tag className={styles['ant-tag']} color="#f50">未通过</Tag>);
     } else if (status === 3) {
-      return (<Tag className={styles['ant-tag']} color="#FFA500">待发布</Tag>);
+      return (<Tag className={styles['ant-tag']} color="#8A2BE2">待发布</Tag>);
     } else if (status === 4) {
-      return (<Tag className={styles['ant-tag']} color="#108ee9">已发布</Tag>);
+      return (<Tag className={styles['ant-tag']} color="#FFC125">已发布</Tag>);
     } else if (status === 5) {
       return (<Tag className={styles['ant-tag']} color="#87d068">淘宝通过</Tag>);
     } else if (status === 6) {
