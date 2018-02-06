b3e3ddc70d597493ed00ee1ebd22aefdb28e665b
焦雁飞 <911306137@qq.com>
2018-02-06 10:01:52 +0800

    修改DetailPane.js

--- src/components/DockPanel/panes/DetailPane.js
+++ src/components/DockPanel/panes/DetailPane.js
@@ -12,45 +12,47 @@ export default class DetailPane extends PureComponent {
       'lineHeight': '40px',
     };
     const labelStyle = { textAlign: 'right' };
+    const labelSpan = {md: 4, sm: 12, xs: 12};
+    const valueSpan = {md: 20, sm: 12, xs: 12};
     return (
       <Card bordered={false} style={{ padding: '0 0 40px' }}>
           <Row gutter={2} style={rowStyle}>
-            <Col span={2} style={labelStyle}>
+            <Col {...labelSpan} style={labelStyle}>
               任务标题：
             </Col>
-            <Col span={22}>
+            <Col {...valueSpan}>
               <div>{task.name || '无'}</div>
             </Col>
           </Row>
           <Row gutter={2} style={rowStyle}>
-            <Col span={2} style={labelStyle}>
+            <Col {...labelSpan} style={labelStyle}>
               任务描述：
             </Col>
-            <Col span={22}>
+            <Col {...valueSpan}>
               <div style={{ wordWrap: 'break-word' }}>{task.desc || '无'}</div>
             </Col>
           </Row>
           <Row gutter={2} style={rowStyle}>
-            <Col span={2} style={labelStyle}>
+            <Col {...labelSpan} style={labelStyle}>
               渠道：
             </Col>
-            <Col span={22}>
+            <Col {...valueSpan}>
               <div>{task.channel_name || '无'}</div>
             </Col>
           </Row>
           <Row gutter={2} style={rowStyle}>
-            <Col span={2} style={labelStyle}>
+            <Col {...labelSpan} style={labelStyle}>
               商家标签：
             </Col>
-            <Col span={22}>
+            <Col {...valueSpan}>
               <div>{task.merchant_tag || '无'}</div>
             </Col>
           </Row>
           <Row gutter={2} style={rowStyle}>
-            <Col span={2} style={labelStyle}>
+            <Col {...labelSpan} style={labelStyle}>
               附 件：
             </Col>
-            <Col span={22}>
+            <Col {...valueSpan}>
               { task.attachments && task.attachments.length > 0 ?
                 task.attachments.map(item => <a target="_blank" href={item.url}>{item.name}</a>)
                 : <span>无</span>
@@ -58,18 +60,18 @@ export default class DetailPane extends PureComponent {
             </Col>
           </Row>
           <Row gutter={2} style={rowStyle}>
-            <Col span={2} style={labelStyle}>
+            <Col {...labelSpan} style={labelStyle}>
               活动酬劳：
             </Col>
-            <Col span={22}>
+            <Col {...valueSpan}>
               <div>{`￥${task.price}`}</div>
             </Col>
           </Row>
           <Row gutter={2} style={rowStyle}>
-            <Col span={2} style={labelStyle}>
+            <Col {...labelSpan} style={labelStyle}>
               创建时间：
             </Col>
-            <Col span={22}>
+            <Col {...valueSpan}>
               <div>{moment(task.create_time).format('YYYY-MM-DD HH:mm')}</div>
             </Col>
           </Row>
