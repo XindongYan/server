052de56a279d4450d50971495a865cc3f01c2861
焦雁飞 <911306137@qq.com>
2018-02-07 18:57:42 +0800

    修改登录跳转参数

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 xinyu-web
 发布
 npm run build
-scp -r ./dist/index.js ./dist/index.css root@120.27.215.205:/data/wwwroot/default/xinyu/xinyu_back/xinyu-prd/public/
+scp -r ./dist/index.js ./dist/index.css root@120.27.215.205:/data/wwwroot/default/xinyu/xinyu_back/xinyu-web/dist/
+scp -r ./dist/index.js ./dist/index.css root@120.27.215.205:/data/wwwroot/default/xinyu/xinyu_back/xinyu-web-prd/dist/
--- src/routes/User/Login.js
+++ src/routes/User/Login.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Icon } from 'antd';
 import styles from './Login.less';
 import { ORIGIN } from '../../constants';
+import querystring from 'querystring';
 
 export default class Login extends Component {
   state = {
@@ -23,7 +24,7 @@ export default class Login extends Component {
       <div className={styles.main}>
         <div>
           <div style={{ marginBottom: 20 }} className={styles.desc}>使用淘宝授权登录</div>
-          <a href={`https://oauth.taobao.com/authorize?client_id=23670142&response_type=code&redirect_uri=${ORIGIN}/api/taobao/auth&state=${encodeURIComponent(JSON.stringify(state))}&view=web`}>
+          <a href={`https://oauth.taobao.com/authorize?client_id=23670142&response_type=code&redirect_uri=${ORIGIN}/api/taobao/auth&state=${encodeURIComponent(querystring.stringify(state))}&view=web`}>
             <Icon type="taobao-circle"  className={styles.iconTaobao}/>
           </a>
         </div>
