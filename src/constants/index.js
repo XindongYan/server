c926df175c3867d183f41859a9f98a3e78127da9
焦雁飞 <911306137@qq.com>
2018-02-07 18:08:23 +0800

    配置编译选项

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,8 @@
 export const ORIGIN = {
   production: 'http://api.nicai360.com',
   test: 'http://testapi.nicai360.com',
-  development: 'http://testapi.nicai360.com', //`http://${location.hostname}:3000`,
+  development: 'http://testapi.nicai360.com',
+  // development: `http://${location.hostname}:3000`,
 }[process.env.NODE_ENV || 'development'];
 
 export const RIGHTS = [
