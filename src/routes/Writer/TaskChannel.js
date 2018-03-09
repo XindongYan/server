b3240abb688e76aa254e4c6e0e37c771ea7f5ff2
焦雁飞 <911306137@qq.com>
2018-03-09 14:58:38 +0800

    微淘

--- src/routes/Writer/TaskChannel.js
+++ src/routes/Writer/TaskChannel.js
@@ -2,11 +2,43 @@ import React, { PureComponent } from 'react';
 import { connect } from 'dva';
 import { routerRedux } from 'dva/router';
 import querystring from 'querystring';
-import { Card, Button, Popconfirm, message, Row, Col } from 'antd';
+import url from 'url';
+import { Card, Button, Popconfirm, message, Row, Col, Menu } from 'antd';
 import TaskChat from '../../components/TaskChat';
 import styles from './TaskChannel.less';
 import { CHANNELS } from '../../constants/taobao';
 import { TASK_TYPES } from '../../constants';
+
+const SubMenu = Menu.SubMenu;
+
+function BlockCommon(props) {
+  const { btnText, btnUrl, img, infoList, title } = props;
+  return (
+    <div className={styles['creator-creation-block-common']} style={{ width: 488, marginBottom: 20 }}>
+      <div className={styles['block-common-img']}><img src={img}/></div>
+      <div className={styles['block-common-detail']}>
+      <h3>{title}</h3>
+      {infoList.map((item, index) =>
+        <div className={styles['block-common-item']}>
+          <label>{item.label}</label>
+          {item.textList.map((item1, index1) =>
+            item1.url ?
+            <div className={styles['block-common-item-desc']}>
+              <a href={item1.url} target="_blank" style={{ fontSize: 12, display: 'block' }}>{item1.text}</a>
+            </div>:
+            <div className={styles['block-common-item-desc']}>
+              {item1.text}
+            </div>
+          )}
+        </div>
+      )}
+      <Button type="primary" onClick={() => props.onDeliver(btnUrl)} className={styles['block-common-btn']}>
+        {btnText}
+      </Button>
+      </div>
+    </div>
+  );
+}
 
 
 @connect(state => ({
@@ -16,6 +48,7 @@ import { TASK_TYPES } from '../../constants';
 export default class TaskChannel extends PureComponent {
   state = {
     data: [],
+    currentKey: 'post',
   }
   async componentDidMount() {
     const data = await fetch(`/jsons/taskChannel.json`).then(response => response.json());
@@ -23,10 +56,13 @@ export default class TaskChannel extends PureComponent {
       data,
     });
   }
-  componentWillReceiveProps(nextProps) {
 
+  handleDeliverWeitao = (btnUrl) => {
+    const query = querystring.parse(this.props.location.search.substr(1));
+    const urlObj = url.parse(btnUrl);
+    const tempQuery = querystring.parse(urlObj.query);
+    this.props.dispatch(routerRedux.push(`/writer/task/create?channelId=${query.channelId}&activityId=${query.activityId}&template=${tempQuery.template}&from=${tempQuery.from}`));
   }
-
   handleDeliver = (template) => {
     const query = querystring.parse(this.props.location.search.substr(1));
     this.props.dispatch(routerRedux.push(`/writer/task/create?channelId=${query.channelId}&activityId=${query.activityId}&template=${template}`));
@@ -54,8 +90,13 @@ export default class TaskChannel extends PureComponent {
       </div>
     )
   }
+  handleClick = (e) => {
+    this.setState({
+      currentKey: e.key,
+    });
+  }
   renderWeitaoChannel = () => {
-    const data = {
+    const weitao = {
       "description": "内容质量与粉丝效果好的微淘可以得到更多频道曝光",
       "linkText": "模板使用帮助",
       "linkUrl": "https://docs.alipay.com/alibaba_we_guide/guide/lbdcfg",
@@ -80,96 +121,96 @@ export default class TaskChannel extends PureComponent {
             }],
             "title": "长文章"
           }
-        }, {
-          "component": "BlockCommon",
-          "props": {
-            "btnText": "立即创作",
-            "btnUrl": "//we.taobao.com/creation2/post?template=itemrank&from=draft",
-            "img": "//img.alicdn.com/tfs/TB1tHkkXmBYBeNjy0FeXXbnmFXa-280-498.png",
-            "infoList": [{
-              "label": "模板描述：",
-              "textList": [{
-                "text": "针对某一主题快速生成有明确排行理由的商品榜单内容，有助于粉丝进行同类型的商品对比，快速种草和产生消费行为"
-              }]
-            }, {
-              "label": "创作指导：",
-              "textList": [{
-                "text": "商品排行内容怎么写？",
-                "url": "https://docs.alipay.com/alibaba_we_guide/guide/pmhg3s"
-              }]
-            }],
-            "title": "商品排行"
-          }
-        }, {
-          "component": "BlockCommon",
-          "props": {
-            "btnText": "立即创作",
-            "btnUrl": "//we.taobao.com/creation2/post?template=iteminventory&from=draft",
-            "img": "//img.alicdn.com/tfs/TB1sbkkXmBYBeNjy0FeXXbnmFXa-280-498.png",
-            "infoList": [{
-              "label": "模板描述：",
-              "textList": [{
-                "text": "针对某一细分场景、主题非常契合的商品进行推荐。好的内容将帮助粉丝对该主题下的商品产生购买行为"
-              }]
-            }, {
-              "label": "创作指导：",
-              "textList": [{
-                "text": "商品推荐内容应该怎么写？",
-                "url": "https://docs.alipay.com/alibaba_we_guide/guide/oqa3o2"
-              }]
-            }],
-            "title": "商品推荐"
-          }
-        }, {
-          "component": "BlockCommon",
-          "props": {
-            "btnText": "立即创作",
-            "btnUrl": "//we.taobao.com/creation2/post?template=shopinventory&from=draft",
-            "img": "//img.alicdn.com/tfs/TB1pI1CXh9YBuNjy0FfXXXIsVXa-280-498.png",
-            "infoList": [{
-              "label": "模板描述：",
-              "textList": [{
-                "text": "针对某一场景、主题的同类型店铺推荐内容，好的内容将帮助粉丝更好的了解对比同类型的店铺"
-              }]
-            }, {
-              "label": "创作指导：",
-              "textList": [{
-                "text": "店铺推荐内容应该怎么写？",
-                "url": "https://docs.alipay.com/alibaba_we_guide/guide/fdz9hu"
-              }]
-            }],
-            "title": "店铺推荐"
-          }
+        // }, {
+        //   "component": "BlockCommon",
+        //   "props": {
+        //     "btnText": "立即创作",
+        //     "btnUrl": "//we.taobao.com/creation2/post?template=itemrank&from=draft",
+        //     "img": "//img.alicdn.com/tfs/TB1tHkkXmBYBeNjy0FeXXbnmFXa-280-498.png",
+        //     "infoList": [{
+        //       "label": "模板描述：",
+        //       "textList": [{
+        //         "text": "针对某一主题快速生成有明确排行理由的商品榜单内容，有助于粉丝进行同类型的商品对比，快速种草和产生消费行为"
+        //       }]
+        //     }, {
+        //       "label": "创作指导：",
+        //       "textList": [{
+        //         "text": "商品排行内容怎么写？",
+        //         "url": "https://docs.alipay.com/alibaba_we_guide/guide/pmhg3s"
+        //       }]
+        //     }],
+        //     "title": "商品排行"
+        //   }
+        // }, {
+        //   "component": "BlockCommon",
+        //   "props": {
+        //     "btnText": "立即创作",
+        //     "btnUrl": "//we.taobao.com/creation2/post?template=iteminventory&from=draft",
+        //     "img": "//img.alicdn.com/tfs/TB1sbkkXmBYBeNjy0FeXXbnmFXa-280-498.png",
+        //     "infoList": [{
+        //       "label": "模板描述：",
+        //       "textList": [{
+        //         "text": "针对某一细分场景、主题非常契合的商品进行推荐。好的内容将帮助粉丝对该主题下的商品产生购买行为"
+        //       }]
+        //     }, {
+        //       "label": "创作指导：",
+        //       "textList": [{
+        //         "text": "商品推荐内容应该怎么写？",
+        //         "url": "https://docs.alipay.com/alibaba_we_guide/guide/oqa3o2"
+        //       }]
+        //     }],
+        //     "title": "商品推荐"
+        //   }
+        // }, {
+        //   "component": "BlockCommon",
+        //   "props": {
+        //     "btnText": "立即创作",
+        //     "btnUrl": "//we.taobao.com/creation2/post?template=shopinventory&from=draft",
+        //     "img": "//img.alicdn.com/tfs/TB1pI1CXh9YBuNjy0FfXXXIsVXa-280-498.png",
+        //     "infoList": [{
+        //       "label": "模板描述：",
+        //       "textList": [{
+        //         "text": "针对某一场景、主题的同类型店铺推荐内容，好的内容将帮助粉丝更好的了解对比同类型的店铺"
+        //       }]
+        //     }, {
+        //       "label": "创作指导：",
+        //       "textList": [{
+        //         "text": "店铺推荐内容应该怎么写？",
+        //         "url": "https://docs.alipay.com/alibaba_we_guide/guide/fdz9hu"
+        //       }]
+        //     }],
+        //     "title": "店铺推荐"
+        //   }
         }],
         "icon": "//gw.alicdn.com/tfscom/TB1OyT.RVXXXXcpXXXXXXXXXXXX.png",
         "title": "帖子",
         "key": "post"
       }, {
-        "blockList": [{
-          "component": "BlockCommon",
-          "props": {
-            "btnText": "立即创作",
-            "btnUrl": "//we.taobao.com/creation/post?template=video2&from=draft",
-            "img": "//img.alicdn.com/tfs/TB1hlTbXeuSBuNjy1XcXXcYjFXa-280-498.png",
-            "infoList": [{
-              "label": "模板描述：",
-              "textList": [{
-                "text": "创作自由度高，优质多样的视频内容将提升粉丝的浏览和受欢迎度"
-              }]
-            }, {
-              "label": "创作指导：",
-              "textList": [{
-                "text": "如何发一篇优质短视频内容？",
-                "url": "https://docs.alipay.com/alibaba_we_guide/guide/qcuk4n"
-              }]
-            }],
-            "title": "短视频"
-          }
-        }],
-        "icon": "//gw.alicdn.com/tfscom/TB1toY.RVXXXXcuXXXXXXXXXXXX.png",
-        "title": "短视频",
-        "key": "video"
-      }, {
+      //   "blockList": [{
+      //     "component": "BlockCommon",
+      //     "props": {
+      //       "btnText": "立即创作",
+      //       "btnUrl": "//we.taobao.com/creation/post?template=video2&from=draft",
+      //       "img": "//img.alicdn.com/tfs/TB1hlTbXeuSBuNjy1XcXXcYjFXa-280-498.png",
+      //       "infoList": [{
+      //         "label": "模板描述：",
+      //         "textList": [{
+      //           "text": "创作自由度高，优质多样的视频内容将提升粉丝的浏览和受欢迎度"
+      //         }]
+      //       }, {
+      //         "label": "创作指导：",
+      //         "textList": [{
+      //           "text": "如何发一篇优质短视频内容？",
+      //           "url": "https://docs.alipay.com/alibaba_we_guide/guide/qcuk4n"
+      //         }]
+      //       }],
+      //       "title": "短视频"
+      //     }
+      //   }],
+      //   "icon": "//gw.alicdn.com/tfscom/TB1toY.RVXXXXcuXXXXXXXXXXXX.png",
+      //   "title": "短视频",
+      //   "key": "video"
+      // }, {
         "blockList": [{
           "component": "BlockCommon",
           "props": {
@@ -250,41 +291,64 @@ export default class TaskChannel extends PureComponent {
         },
         "title": "单品",
         "key": "item"
-      }, {
-        "blockList": [{
-          "component": "BlockCommon",
-          "props": {
-            "btnText": "立即创作",
-            "btnUrl": "//we.taobao.com/creation/post?template=qa&from=draft",
-            "img": "//img.alicdn.com/tfs/TB1rbbXXkyWBuNjy0FpXXassXXa-280-498.png",
-            "infoList": [{
-              "label": "模板描述：",
-              "textList": [{
-                "text": "发起一场问题答疑的互动，和粉丝近距离的互动交流，帮助你更好的了解粉丝需要什么。"
-              }]
-            }, {
-              "label": "创作指导：",
-              "textList": [{
-                "text": "如何做好一场问答？",
-                "url": "https://docs.alipay.com/alibaba_we_guide/guide/cl6tv5"
-              }]
-            }],
-            "title": "互动问答"
-          }
-        }],
-        "icon": "//gw.alicdn.com/tfs/TB1pHQHff6H8KJjy0FjXXaXepXa-140-140.png",
-        "title": "问答",
-        "key": "answer"
+      // }, {
+      //   "blockList": [{
+      //     "component": "BlockCommon",
+      //     "props": {
+      //       "btnText": "立即创作",
+      //       "btnUrl": "//we.taobao.com/creation/post?template=qa&from=draft",
+      //       "img": "//img.alicdn.com/tfs/TB1rbbXXkyWBuNjy0FpXXassXXa-280-498.png",
+      //       "infoList": [{
+      //         "label": "模板描述：",
+      //         "textList": [{
+      //           "text": "发起一场问题答疑的互动，和粉丝近距离的互动交流，帮助你更好的了解粉丝需要什么。"
+      //         }]
+      //       }, {
+      //         "label": "创作指导：",
+      //         "textList": [{
+      //           "text": "如何做好一场问答？",
+      //           "url": "https://docs.alipay.com/alibaba_we_guide/guide/cl6tv5"
+      //         }]
+      //       }],
+      //       "title": "互动问答"
+      //     }
+      //   }],
+      //   "icon": "//gw.alicdn.com/tfs/TB1pHQHff6H8KJjy0FjXXaXepXa-140-140.png",
+      //   "title": "问答",
+      //   "key": "answer"
       }],
       "title": "发布新微淘"
     };
+    const activity = weitao.tabList.find(item => item.key === this.state.currentKey);
     return (
       <div>
-        <div style={{background: '#fff', padding: 20}}>
-          <span style={{fontSize: 16, marginRight: 20}}>{data.title}</span>
-          <span style={{fontSize: 12, color: '#999'}}>{data.description}</span>
-
-        </div>
+        <Card bodyStyle={{ paddingBottom: 0 }}>
+          <span style={{fontSize: 16, marginRight: 20}}>{weitao.title}</span>
+          <span style={{fontSize: 12, color: '#999'}}>{weitao.description} <a target="_blank" href={weitao.linkUrl}>{weitao.linkText}</a></span>
+          <Menu
+            style={{ marginTop: 20, borderBottom: 'none' }}
+            onClick={this.handleClick}
+            selectedKeys={[this.state.currentKey]}
+            mode="horizontal"
+          >
+            {weitao.tabList.map(item =>
+              <Menu.Item key={item.key}>
+                <img style={{ width: 30, marginRight: 10 }} src={item.icon}/>{item.title}
+              </Menu.Item>)}
+          </Menu>
+        </Card>
+        <Card style={{ marginTop: 20 }}>
+          <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}>
+            {activity.blockList.map((item, index) => {
+              if (item.component === 'BlockCommon') {
+                return <BlockCommon key={index} {...item.props} onDeliver={this.handleDeliverWeitao}/>;
+              } else if (item.component === 'BlockPublishItemGroup') {
+                return null;
+              }
+              return null;
+            })}
+          </div>
+        </Card>
       </div>
     );
   }
