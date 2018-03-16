5f501dae1b9e7c4bbac6f05e011db72408d17b2c
apple <apple@xiaoydeMacBook-Pro.local>
2018-03-16 15:53:48 +0800

    有好货 标准段落

--- src/components/Form/StructCanvas/BodyStructContent.js
+++ src/components/Form/StructCanvas/BodyStructContent.js
@@ -1,11 +1,12 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'dva';
-import { Input, Icon, message, Row, Col, Tag, Button, Form } from 'antd';
+import { Input, Icon, message, Row, Col, Tag, Button, Form, Select } from 'antd';
 import styles from '../index.less';
 import AlbumModal from '../../AlbumModal';
 
 const FormItem = Form.Item;
 const { TextArea } = Input;
+const Option = Select.Option;
 @connect(state => ({
 
 }))
@@ -17,12 +18,12 @@ export default class BodyStructContent extends PureComponent {
     images: [],
   }
   componentDidMount() {
-    if (this.props.value && this.props.value.title) {
-      const { value } = this.props;
+    if (this.props.value && this.props.value.data && this.props.value.data.title) {
+      const { value: { data } } = this.props;
       this.setState({
-        title: value.title,
-        desc: value.desc,
-        images: value.images,
+        title: data.title,
+        desc: data.desc,
+        images: data.images,
       }, () => {
         const fieldsValue = {
           title: this.state.title,
@@ -86,34 +87,52 @@ export default class BodyStructContent extends PureComponent {
   }
 
   render() {
-    const { value, operation, form: { getFieldDecorator } } = this.props;
+    const { props, value, operation, form: { getFieldDecorator } } = this.props;
     const { images } = this.state;
+    const titleRules = value.moduleInfo.dataSchema.properties.title;
+    const descRules = value.moduleInfo.dataSchema.properties.desc;
     return (
       <div style={{ padding: '0 0 20px'}}>
-        <div>
+        <div style={{marginBottom: 20}}>
           <p className={styles.lineTitleDefult}>
-            自定义段落标题:
+            {titleRules.title || ''}
           </p>
           <div className={styles.InputBox}>
-            <FormItem>
-              {getFieldDecorator('title', {
-                rules: [{
-                  required: true, message: '不能为空',
-                }, {
-                  min: 4, message: '输入内容长度不能小于 4',
-                }, {
-                  whitespace: true, message: '内容不能为空格'
-                }],
-              })(
-                <Input
-                  maxLength="6"
-                  suffix={<span>{ this.props.form.getFieldValue('title') ? this.props.form.getFieldValue('title').length : 0 }/6</span>}
-                  placeholder="请在这里输入4-6个字以内的段落标题"
-                />
-              )}
-            </FormItem>
+            { value.name === 'item-paragraph' ? <FormItem>
+                {getFieldDecorator('title', {
+                  rules: [{
+                    required: true, message: '不能为空',
+                  }, {
+                    min: titleRules.minLength, message: `输入内容长度不能小于${titleRules.minLength}`,
+                  }, {
+                    max: titleRules.maxLength, message: `输入内容长度不能大于${titleRules.maxLength}`,
+                  }, {
+                    whitespace: true, message: '内容不能为空格'
+                  }],
+                })(
+                  <Input
+                    maxLength={titleRules.maxLength ? titleRules.maxLength.toString() : '6'}
+                    suffix={<span>{ this.props.form.getFieldValue('title') ? this.props.form.getFieldValue('title').length : 0 }/{titleRules.maxLength}</span>}
+                    placeholder={titleRules['ui:placeholder']}
+                  />
+                )}
+              </FormItem> :
+              <FormItem>
+                {getFieldDecorator('title', {
+                  rules: [{
+                    required: true, message: '不能为空',
+                  }],
+                })(
+                  <Select style={{ minWidth: 120 }}>
+                    {props.channelTitle && props.channelTitle.dataSource ?
+                      props.channelTitle.dataSource.map(item => <Option value={item.value}>{item.label}</Option>): ''
+                    }
+                  </Select>
+                )}
+              </FormItem>
+            }
           </div>
-          <p className={styles.promptText}>请在这里输入4-6个字以内的段落标题</p>
+          <p className={styles.promptText}>{titleRules.description || ''}</p>
         </div>
         <div>
           <p className={styles.lineTitleDefult}>
@@ -125,21 +144,20 @@ export default class BodyStructContent extends PureComponent {
                 rules: [{
                   required: true, message: '不能为空',
                 }, {
-                  min: 60, message: '文字长度太短, 要求长度最少为60',
+                  min: descRules.minLength, message: `文字长度太短, 要求长度最少为${descRules.minLength}`,
                 }, {
-                  max: 200, message: '文字长度太长, 要求长度最多为200',
+                  max: descRules.maxLength, message: `文字长度太长, 要求长度最多为${descRules.maxLength}`,
                 }, {
                   whitespace: true, message: '内容不能为空格'
                 }],
               })(
                 <TextArea
-                  maxLength="200"
-                  placeholder="请在这里输入60-200字文本段落描述">
+                  maxLength={descRules.maxLength ? descRules.maxLength.toString() : '200'}
+                  placeholder={descRules['ui:placeholder']}>
                 </TextArea>
               )}
             </FormItem>
           </div>
-          <p className={styles.promptText}>请在这里输入60-200字文本段落描述</p>
         </div>
         <div>
           <p className={styles.lineTitleDefult}>
